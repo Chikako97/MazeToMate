afd6ca7f7f146059d0f8b92d38d3d289ccf7604c
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-01-05 15:47:36 +0100

    Verbindung der Spielwelten = Aufzug (TK)

---
+++ Aufzug.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aufzug : MonoBehaviour
+{
+
+    public GameObject AufzugLinks;
+    public GameObject AufzugRechts;
+    public GameObject AufzugVorn;
+
+    bool bewegung;
+    float Gesamtzeit;
+    Vector3 startPos;
+    Vector3 endPos;
+    Vector3 Gesamtstrecke;
+    float bewegungZeit;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Bewegung der Aufzüge
+        bewegung = false;
+        Gesamtzeit = 5;
+        startPos = new Vector3(0, 0.09f, 0);
+        endPos = new Vector3(0, 2, 0);
+        Gesamtstrecke = endPos - startPos;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && !bewegung)
+        {
+            bewegung = true;
+            bewegungZeit = Time.time;
+        }
+
+        if (bewegung)
+        {
+            float zeit = (Time.time - bewegungZeit) / Gesamtzeit;
+            Vector3 Strecke = zeit * Gesamtstrecke;
+            transform.position = startPos + Strecke;
+            transform.position = startPos + Strecke;
+            transform.position = startPos + Strecke;
+
+            if (zeit >= 1)
+            {
+                bewegung = false;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D) && !bewegung)
+        {
+            bewegung = true;
+            bewegungZeit = Time.time;
+        }
+
+        else if (bewegung)
+        {
+            float zeit = (Time.time - bewegungZeit) / Gesamtzeit;
+            Vector3 Strecke = zeit * Gesamtstrecke;
+            transform.position = endPos - Strecke;
+            transform.position = endPos - Strecke;
+            transform.position = endPos - Strecke;
+
+            if (zeit >= 1)
+            {
+                bewegung = false;
+            }
+        }
+    }
+}
