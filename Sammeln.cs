18a23095ca6354602cbcea0174feccbbb449ab7b
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-01-05 15:49:51 +0100

    Funktion zum einsammeln der Tropfen (TK)

---
+++ Sammeln.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sammeln : MonoBehaviour
+{
+
+    public GameObject ps;
+
+    GameObject Tropfen;
+    int TropfenStatus = 0;
+    public Material blau;
+    public Material rot;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Funktion, um Tropfen aufzusammeln und Farbe auf Blau zu ändern
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tropfen"))
+        {
+            other.gameObject.SetActive(false);
+            Instantiate(ps, transform.position, Quaternion.identity);
+            gameObject.GetComponent<Renderer>().material = blau;
+
+        } else {
+            gameObject.GetComponent<Renderer>().material = rot;
+        }
+    }
+}
