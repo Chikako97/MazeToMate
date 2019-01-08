071d406faf5f0a865a5766e323536b86457bc4d0
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-01-08 12:22:16 +0100

    Welt 1 vorläufig (TK)

--- Welt.cs
+++ Welt.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Welt : MonoBehaviour
 {
@@ -9,9 +11,25 @@ public class Welt : MonoBehaviour
     private float speed = 10f;
     private float sprungkraft = 7f;
     private float gravity = 30f;
-    private Vector3 moveDir = Vector3.zero;
+    private Vector3 moveDir = Vector3.zero; 
     bool weltStehe = false;
+    float translateFaktor = 3;
+    float rotateFaktor = 100;
 
+    GameObject Durchgang;
+    int durchgangStatus = 0;  // Status = 0 besagt, dass Spieler nichts berührt bzw alle Tore geschlossen sind
+    public Material schwarzMat;
+    public Material rot;
+
+    GameObject truhe;
+    int truheIndex;
+    int truheStatus = 0;    // Status = 0 besagt, dass Spieler nichts berührt bzw alle Tore geschlossen sind
+    public GameObject[] schluessel = new GameObject[12];
+    public GameObject[] besitz = new GameObject[12];
+
+    int sperreStatus = 0;   // Status = 0 besagt, dass Spieler nichts berührt bzw alle Tore geschlossen sind
+    int sperreIndex;
+    public GameObject[] sperre = new GameObject[8];
 
     void Start()
     {
@@ -88,10 +106,59 @@ public class Welt : MonoBehaviour
     void Update()
     {
 
-      
+        // Status 2 = "Oeffne Tür" wurde aktiviert und es oeffnet sich selbstständig und Spieler entfärbt sich und kann weiter gehen
+        if (durchgangStatus == 2)
+        {
+            Durchgang.transform.position = Durchgang.transform.position + new Vector3(0, Time.deltaTime, 0);
+            if (Durchgang.transform.position.y >= 1.5f)
+            {
+                durchgangStatus = 3;
+                weltStehe = false;
+                gameObject.GetComponent<Renderer>().material = rot;
+                Invoke("TorSchliesst", 2.5f);
+            }
+        }
+        // Tür schließt sich
+        else if (durchgangStatus == 4)
+        {
+            Durchgang.transform.position = Durchgang.transform.position - new Vector3(0, Time.deltaTime, 0);
+            if (Durchgang.transform.position.y <= 0.5f)
+            {
+                durchgangStatus = 0;
+            }
+        }
+        // Status 2 = "Oeffne truhe" wurde aktiviert und es oeffnet sich selbstständig und Spieler entfärbt sich und kann weiter gehen
+        if (truheStatus == 2)
+        {
+            truhe.transform.position = truhe.transform.position + new Vector3(0, Time.deltaTime, 0);
+            if (truhe.transform.position.y >= 1.1f)
+            {
+                //Das Tor ist für 2 Sekunden geoeffnet
+                truheStatus = 3;
+                Invoke("KisteSchliesst", 2.5f);
+                if (besitz[truheIndex].activeInHierarchy)
+                {
+                    weltStehe = false;
+                    gameObject.GetComponent<Renderer>().material = rot;
+                }
+            }
+        }
+        // Truhe schließt sich wieder 
+        else if (truheStatus == 4)
+        {
+            truhe.transform.position = truhe.transform.position - new Vector3(0, Time.deltaTime, 0);
+            if (truhe.transform.position.y <= 0.5f)
+            {
+                truheStatus = 0;
+            }
+        }
+
         //Steuerung der Spielfigur
         if (!weltStehe)
         {
+            transform.Translate(new Vector3(0, 0, translateFaktor * Time.deltaTime * Input.GetAxis("Vertical")));
+            transform.Rotate(new Vector3(0, rotateFaktor * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
+
             CharacterController controller = gameObject.GetComponent<CharacterController>();
 
             if (controller.isGrounded)
@@ -99,6 +166,7 @@ public class Welt : MonoBehaviour
                 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
                 moveDir = transform.TransformDirection(moveDir);
                 moveDir *= speed;
+
                 // Figur zum springen bringen
                 if (Input.GetButtonUp("Jump"))
                 {
@@ -107,7 +175,79 @@ public class Welt : MonoBehaviour
             }
             moveDir.y -= gravity * Time.deltaTime;
             controller.Move(moveDir * Time.deltaTime);
+        } 
+    }
+
+    // Reaktion bei Beruehrung wird ausgelöst = Spieler wird betäubt und verfärtb
+    void OnTriggerEnter(Collider other, Collider coll)
+    {
+        if (coll.gameObject.tag == "Tor")
+        {
+            Durchgang = coll.gameObject;
+            durchgangStatus = 1;
+            weltStehe = true;
+            gameObject.GetComponent<Renderer>().material = schwarzMat;
+        }
+        else if (coll.gameObject.tag == "Truhe")
+        {
+            truhe = coll.gameObject;
+            truheIndex = System.Convert.ToInt32(truhe.name.Substring(5, 1));
+            truheStatus = 1;
+            weltStehe = true;
+            gameObject.GetComponent<Renderer>().material = schwarzMat;
+        }
+        else if (coll.gameObject.tag == "Sperre")
+        {
+            sperreIndex = System.Convert.ToInt32(coll.gameObject.name.Substring(6, 1));
+            sperreStatus = 1;
+            weltStehe = true;
+            gameObject.GetComponent<Renderer>().material = schwarzMat;
         }
     }
 
+    void TorSchliesst()
+    {
+        durchgangStatus = 4;
+    }
+
+    public void OeffneTor()
+    {
+        if (durchgangStatus == 1)
+        {                               //Spieler wird betäubt und wechselt farbe
+            durchgangStatus = 2;
+        }
+    }
+
+    void TruheSchliesst()
+    {
+        truheStatus = 4;
+    }
+
+    public void OeffneTruhe()
+    {
+        if (truheStatus == 1)
+        {
+            truheStatus = 2;
+        }
+    }
+
+    public void NimmSchluesselAuf()
+    {
+        if (truheStatus == 3)
+        {
+            besitz[truheIndex].SetActive(true);
+            schluessel[truheIndex].SetActive(false);
+            weltStehe = false;
+            gameObject.GetComponent<Renderer>().material = rot;
+        }
+    }
+
+    public void SchliesseSperreAuf()
+    {
+        if (sperreStatus == 1 && besitz[sperreIndex].activeInHierarchy) 
+            sperre[sperreIndex].SetActive(false);
+        sperreStatus = 0;
+        weltStehe = false;
+        gameObject.GetComponent<Renderer>().material = rot;
+    }
 }
