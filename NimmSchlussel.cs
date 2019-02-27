a2f2a8f7b135f61c23c45e6647553440c1dbf48b
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-02-27 12:24:53 +0100

    Schlüsselmechanismus (TK)

---
+++ NimmSchlussel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NimmSchlussel : MonoBehaviour {
+
+    public Component torschlussel;
+    public Component torschlussel1;
+    public GameObject Schlussel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay () {
+        if (Input.GetKey(KeyCode.P))
+            torschlussel.GetComponent<BoxCollider>().enabled = true;
+            torschlussel1.GetComponent<BoxCollider>().enabled = true;
+
+        if (Input.GetKey(KeyCode.P))
+            Schlussel.SetActive(false);
+	}
+}
