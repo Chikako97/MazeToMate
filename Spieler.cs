66d5df65fb4bdc43d65cfbcbd627b40f6d23065e
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-03-06 16:47:04 +0100

    Spielfigur mit Steuerung und Co

---
+++ Spieler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spieler : MonoBehaviour
+{
+        bool bewegung;
+        float jump;
+        private bool isDead;
+        float translateFaktor = 3;
+        float rotateFaktor = 100;
+        [SerializeField] Rigidbody rb;
+        public GameObject Gameover;
+
+        // Use this for initialization
+        void Start () {
+            bewegung = false;
+            isDead = false;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate () {
+            if (!bewegung)
+            {
+                transform.Translate(new Vector3(0, 0, translateFaktor * Time.deltaTime * Input.GetAxis("Vertical")));
+                transform.Rotate(new Vector3(0, rotateFaktor * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
+
+                if (Input.GetButtonUp("Jump"))
+                {
+                    rb.AddForce(0, 3.5f, 0, ForceMode.Impulse);
+                }
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Abgrund") {
+                RaycastHit hit;
+
+                Ray down = new Ray(transform.position, -Vector3.up);
+
+                if (!Physics.Raycast(down, out hit))
+                {
+                    isDead = true;
+                    Gameover.SetActive(true);
+                    transform.GetChild(0).transform.parent = null;
+                }
+            }
+        }
+}
