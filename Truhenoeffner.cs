5915b7538eedf53c3818c2108260108a1e256c6a
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-03-07 17:44:05 +0100

    TruhenOeffner (TK)

---
+++ Truhenoeffner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truhenoeffner : MonoBehaviour {
+
+    public Animation Truhe;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            Truhe.Play();
+        }
+    }
+}
