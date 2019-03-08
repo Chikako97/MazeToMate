06c0c4218420bb81fd8b6eb9be46a6562f92c357
Sophie <40387683+Overmey@users.noreply.github.com>
2019-03-08 17:02:29 +0100

    Spiel mit Escapetaste beenden (SO)

---
+++ Escape.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Spiel soll mit Escape-Taste beendet werden
+            Debug.Log("Spiel wird beendet");
+            Application.Quit();
+        }
+    }
+}
