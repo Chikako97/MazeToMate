e1bc7ad56a1c6f4530f4e7b5e4182bf7a1ef448b
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-03-08 12:54:07 +0100

    Win condition final (TK)

---
+++ WinCondition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour {
+    public LevelManager levelmanager;
+    public GameObject GewonnenText;
+    // Use this for initialization
+    void Start()
+    {
+        levelmanager = FindObjectOfType<LevelManager>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Spieler")
+        {
+            Debug.Log("Ziel erreicht");
+            GameObject TomatenkumpelGameObject = GameObject.Find("Kumpel");
+            GewonnenText.SetActive(true);
+            levelmanager.RespawnPlayer();
+        }
+    }
+}
