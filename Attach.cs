4108cff2b13123b95d0522ecf192123d2350f592
altietz <40388060+altietz@users.noreply.github.com>
2019-03-09 23:55:33 +0100

    Attach (AT)

---
+++ Attach.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attach : MonoBehaviour {
+
+    public GameObject Spieler;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Spieler)
+        {
+            Spieler.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Spieler)
+        {
+            Spieler.transform.parent = null;
+        }
+    }
+}
