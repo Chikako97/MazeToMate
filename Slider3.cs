5b27dc5296e72bdd56418abefa97fd14b567e4d0
altietz <40388060+altietz@users.noreply.github.com>
2019-03-09 23:47:43 +0100

    Update Slider3 (AT)

--- Slider3.cs
+++ Slider3.cs
@@ -14,8 +14,8 @@ public class Slider3 : MonoBehaviour {
     {
         bewegung = false;
         zeitGesamt = 2.5f;
-        startPunkt = new Vector3(18.01f, 0, 19.2f);
-        Vector3 zielPunkt = new Vector3(40, 0, 19.2f);
+        startPunkt = new Vector3(67.145f, 8.56f, 101);
+        Vector3 zielPunkt = new Vector3(89.11f, 8.56f, 101);
         streckeGesamt = zielPunkt - startPunkt;
     }
 
