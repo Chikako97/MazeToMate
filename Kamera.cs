31ab73704da88a5e33405224a1f1d38d63582b41
Sophie <40387683+Overmey@users.noreply.github.com>
2019-03-09 11:42:30 +0100

    Kamera (SO)

--- Kamera.cs
+++ Kamera.cs
@@ -3,17 +3,16 @@
 public class Kamera : MonoBehaviour
 {
     public GameObject spieler;
-    float abstandXZ = 1;
-    float hoeheY = 0.2f;
+    float abstand = 1;
+    float hoehe = 0.2f;
 
     void Update()
     {
-        Quaternion spielerRotationY = new Quaternion();
-        spielerRotationY.eulerAngles = new Vector3(0, spieler.transform.eulerAngles.y, 0);
-        Vector3 abstandHinterSpieler = spielerRotationY * new Vector3(0, 0, abstandXZ);
-        transform.position = spieler.transform.position - abstandHinterSpieler;
-        transform.position = new Vector3(transform.position.x,
-        transform.position.y + hoeheY, transform.position.z);
+        Quaternion spielerRotation = new Quaternion();
+        spielerRotation.eulerAngles = new Vector3(0, spieler.transform.eulerAngles.y, 0);
+        Vector3 abstandZuSpieler = spielerRotation * new Vector3(0, 0, abstand);
+        transform.position = spieler.transform.position - abstandZuSpieler;
+        transform.position = new Vector3(transform.position.x, transform.position.y + hoehe, transform.position.z);
         transform.LookAt(spieler.transform);
     }
     
