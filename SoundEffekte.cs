7b1cf4a13693c388e3a1cc4ada252d7af1b58f9a
Sophie <40387683+Overmey@users.noreply.github.com>
2019-03-13 11:10:00 +0100

    Soundeffekte (SO)

---
+++ SoundEffekte.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffekte : MonoBehaviour
+{
+
+    public AudioSource levelMusik;
+    public AudioSource gameOverMusik;
+    public AudioSource winMusik;
+
+    public bool levelSong = true;
+    public bool gameOverSong = false;
+    public bool winSong = false;
+
+    public void LevelMusik()
+    {
+        levelSong = true;
+        gameOverSong = false;
+        levelMusik.Play();
+    }
+
+    public void GameOverSound()
+    {
+        if (levelMusik.isPlaying)
+        {
+            levelMusik.Stop();
+            levelSong = false;
+        }
+
+        if (!gameOverMusik.isPlaying && gameOverSong == false)
+        {
+            gameOverMusik.Play();
+            gameOverSong = true;
+        }
+    }
+
+    public void WinSound()
+    {
+        if (levelMusik.isPlaying)
+        {
+            levelMusik.Stop();
+            levelSong = false;
+        }
+
+        if (!winMusik.isPlaying && winSong == false)
+        {
+            winMusik.Play();
+            winSong = true;
+        }
+    }
+}
