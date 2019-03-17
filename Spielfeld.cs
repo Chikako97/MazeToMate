c941cdf9c8c62aa46d59afe96bc247b2a4399ce0
Theresa <40387958+Chikako97@users.noreply.github.com>
2019-03-17 15:47:29 +0100

    Hinzufuegen von Materialien (TK)

--- Spielfeld.cs
+++ Spielfeld.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spielfeld : MonoBehaviour
 {
+    public Material Treppe, boden;
     Shader shader;
     Texture texture;
     Color color;
@@ -13,114 +14,114 @@ public class Spielfeld : MonoBehaviour
     {
         //Bodenplatte von Welt 1 (Labyrinth Theresa K)
         GameObject unten = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend = unten.GetComponent<Renderer>();
+        MeshRenderer rend = unten.GetComponent<MeshRenderer>();
         rend.material = new Material(Shader.Find("Specular"));
-        rend.material.SetColor("_Color", Color.green);
+        rend.material = boden;
         unten.transform.localScale = new Vector3(40, 0.2f, 40);
-        unten.transform.Translate(19.91f, 0, 19.8f);
+        unten.transform.Translate(19.91f, 0, 20);
 
 
         //Start Welt 2
-        /*GameObject boden = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend2 = boden.GetComponent<Renderer>();
-        rend2.material = new Material(Shader.Find("Specular"));
-        rend2.material.SetColor("_Color", Color.green);
-        boden.transform.localScale = new Vector3(8, 0.5f, 8);
-        boden.transform.Translate(63.59f, 8.5f, 21);*/
+        GameObject plattform = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        MeshRenderer rend0 = plattform.GetComponent<MeshRenderer>();
+        rend0.material = new Material(Shader.Find("Specular"));
+        rend0.material = boden;
+        plattform.transform.localScale = new Vector3(8, 0.5f, 8);
+        plattform.transform.Translate(63.59f, 8.5f, 21);
 
         //Treppen von Welt 2 (Labyrinth Alex T)
         GameObject Trepperechts = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend1 = Trepperechts.GetComponent<Renderer>();
+        MeshRenderer rend1 = Trepperechts.GetComponent<MeshRenderer>();
         rend1.material = new Material(Shader.Find("Specular"));
-        rend1.material.SetColor("_Color", Color.gray);
+        rend1.material = Treppe;
         Trepperechts.transform.localScale = new Vector3(3, 0.5f, 5);
         Trepperechts.transform.Translate(58, 7.8f, 21);
         GameObject Trepperechts2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend2 = Trepperechts2.GetComponent<Renderer>();
+        MeshRenderer rend2 = Trepperechts2.GetComponent<MeshRenderer>();
         rend2.material = new Material(Shader.Find("Specular"));
-        rend2.material.SetColor("_Color", Color.gray);
+        rend2.material = Treppe;
         Trepperechts2.transform.localScale = new Vector3(3, 0.5f, 5);
         Trepperechts2.transform.Translate(55, 7, 21);
         GameObject Trepperechts3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend3 = Trepperechts3.GetComponent<Renderer>();
+        MeshRenderer rend3 = Trepperechts3.GetComponent<MeshRenderer>();
         rend3.material = new Material(Shader.Find("Specular"));
-        rend3.material.SetColor("_Color", Color.gray);
+        rend3.material = Treppe;
         Trepperechts3.transform.localScale = new Vector3(3, 0.5f, 5);
         Trepperechts3.transform.Translate(52, 6.2f, 21);
         GameObject Trepperechts4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend4 = Trepperechts4.GetComponent<Renderer>();
+        MeshRenderer rend4 = Trepperechts4.GetComponent<MeshRenderer>();
         rend4.material = new Material(Shader.Find("Specular"));
-        rend4.material.SetColor("_Color", Color.gray);
+        rend4.material = Treppe;
         Trepperechts4.transform.localScale = new Vector3(3, 0.5f, 5);
         Trepperechts4.transform.Translate(49, 5.4f, 21);
         GameObject Trepperechts5 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend5 = Trepperechts5.GetComponent<Renderer>();
+        MeshRenderer rend5 = Trepperechts5.GetComponent<MeshRenderer>();
         rend5.material = new Material(Shader.Find("Specular"));
-        rend5.material.SetColor("_Color", Color.gray);
+        rend5.material = Treppe;
         Trepperechts5.transform.localScale = new Vector3(3, 0.5f, 5);
         Trepperechts5.transform.Translate(46, 4.6f, 21);
         GameObject Trepperechts6 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend6 = Trepperechts6.GetComponent<Renderer>();
+        MeshRenderer rend6 = Trepperechts6.GetComponent<MeshRenderer>();
         rend6.material = new Material(Shader.Find("Specular"));
-        rend6.material.SetColor("_Color", Color.gray);
+        rend6.material = Treppe;
         Trepperechts6.transform.localScale = new Vector3(3, 0.5f, 5);
         Trepperechts6.transform.Translate(43, 3.8f, 21);
         GameObject Trepperechts7 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend7 = Trepperechts7.GetComponent<Renderer>();
+        MeshRenderer rend7 = Trepperechts7.GetComponent<MeshRenderer>();
         rend7.material = new Material(Shader.Find("Specular"));
-        rend7.material.SetColor("_Color", Color.gray);
+        rend7.material = Treppe;
         Trepperechts7.transform.localScale = new Vector3(1.5f, 0.5f, 7);
         Trepperechts7.transform.Translate(40.54f, 3.063f, 21.69f);
 
 
         // Treppe zu Welt 3 (Würfel Sophie O)
         GameObject Treppevorn = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend0 = Treppevorn.GetComponent<Renderer>();
-        rend0.material = new Material(Shader.Find("Specular"));
-        rend0.material.SetColor("_Color", Color.green);
+        MeshRenderer rend10 = Treppevorn.GetComponent<MeshRenderer>();
+        rend10.material = new Material(Shader.Find("Specular"));
+        rend10.material = boden;
         Treppevorn.transform.localScale = new Vector3(15, 0.5f, 30);
         Treppevorn.transform.Translate(19.39f, -10.2f, 73.76f);
 
         GameObject Treppevorn1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend11 = Treppevorn1.GetComponent<Renderer>();
+        MeshRenderer rend11 = Treppevorn1.GetComponent<MeshRenderer>();
         rend11.material = new Material(Shader.Find("Specular"));
-        rend11.material.SetColor("_Color", Color.gray);
+        rend11.material = Treppe;
         Treppevorn1.transform.localScale = new Vector3(6, 0.5f, 3);
         Treppevorn1.transform.Translate(30.07f, 7.73f, 65.1f);
         GameObject Treppevorn2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend12 = Treppevorn2.GetComponent<Renderer>();
+        MeshRenderer rend12 = Treppevorn2.GetComponent<MeshRenderer>();
         rend12.material = new Material(Shader.Find("Specular"));
-        rend12.material.SetColor("_Color", Color.gray);
+        rend12.material = Treppe;
         Treppevorn2.transform.localScale = new Vector3(5, 0.5f, 3);
         Treppevorn2.transform.Translate(29.09f, 7, 62);
         GameObject Treppevorn3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend13 = Treppevorn3.GetComponent<Renderer>();
+        MeshRenderer rend13 = Treppevorn3.GetComponent<MeshRenderer>();
         rend13.material = new Material(Shader.Find("Specular"));
-        rend13.material.SetColor("_Color", Color.gray);
+        rend13.material = Treppe;
         Treppevorn3.transform.localScale = new Vector3(5, 0.5f, 3);
         Treppevorn3.transform.Translate(27.32f, 6, 59);
         GameObject Treppevorn4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend14 = Treppevorn4.GetComponent<Renderer>();
+        MeshRenderer rend14 = Treppevorn4.GetComponent<MeshRenderer>();
         rend14.material = new Material(Shader.Find("Specular"));
-        rend14.material.SetColor("_Color", Color.gray);
+        rend14.material = Treppe;
         Treppevorn4.transform.localScale = new Vector3(5, 0.5f, 3);
         Treppevorn4.transform.Translate(26.18f, 5, 56);
         GameObject Treppevorn5 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend15 = Treppevorn5.GetComponent<Renderer>();
+        MeshRenderer rend15 = Treppevorn5.GetComponent<MeshRenderer>();
         rend15.material = new Material(Shader.Find("Specular"));
-        rend15.material.SetColor("_Color", Color.gray);
+        rend15.material = Treppe;
         Treppevorn5.transform.localScale = new Vector3(5, 0.5f, 3);
         Treppevorn5.transform.Translate(25.15f, 4, 53);
         GameObject Treppevorn6 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend16 = Treppevorn6.GetComponent<Renderer>();
+        MeshRenderer rend16 = Treppevorn6.GetComponent<MeshRenderer>();
         rend16.material = new Material(Shader.Find("Specular"));
-        rend16.material.SetColor("_Color", Color.gray);
+        rend16.material = Treppe;
         Treppevorn6.transform.localScale = new Vector3(5, 0.5f, 3);
         Treppevorn6.transform.Translate(24.4f, 3.5f, 50);
         GameObject Treppevorn7 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Renderer rend17 = Treppevorn7.GetComponent<Renderer>();
+        MeshRenderer rend17 = Treppevorn7.GetComponent<MeshRenderer>();
         rend17.material = new Material(Shader.Find("Specular"));
-        rend17.material.SetColor("_Color", Color.gray);
+        rend17.material = Treppe;
         Treppevorn7.transform.localScale = new Vector3(5, 0.5f, 9);
         Treppevorn7.transform.Translate(25.55f, 3.0f, 44.08f);
     }
