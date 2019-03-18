9b058fdb25f324505d4fb33ce1e9fe9e57f41e1d
altietz <40388060+altietz@users.noreply.github.com>
2019-03-18 14:08:10 +0100

    Update Slider9.cs

--- Slider9.cs
+++ Slider9.cs
@@ -14,8 +14,8 @@ public class Slider9 : MonoBehaviour {
     {
         bewegung = false;
         zeitGesamt = 2.5f;
-        startPunkt = new Vector3(172.135f, 8.56f, 107.31f);
-        Vector3 zielPunkt = new Vector3(194.63f, 8.56f, 81.8f);
+        startPunkt = new Vector3(431.2f, 8.5f, 97.76f);   
+        Vector3 zielPunkt = new Vector3(487.25f, 30, 12.5f);
         streckeGesamt = zielPunkt - startPunkt;
     }
 
