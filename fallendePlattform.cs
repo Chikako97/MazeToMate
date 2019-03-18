fbc507e61ec491973f48cffa547847333425aa04
Sophie <40387683+Overmey@users.noreply.github.com>
2019-03-18 20:49:14 +0100

    fallende Plattformen (SO)

---
+++ fallendePlattform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallendePlattform : MonoBehaviour {
+
+    bool faellt = false;
+    float fallGeschw = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (collider.gameObject.name == "Spieler")
+        {
+            faellt = true;
+            Destroy(gameObject, 10);
+        }
+    }
+
+    private void Update()
+    {
+        if (faellt)
+        {
+            fallGeschw += Time.deltaTime;
+
+            transform.position = new Vector3(transform.position.x, transform.position.y - fallGeschw, transform.position.z);
+        }
+    }
+}
